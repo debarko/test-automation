353c3d7dab9d605f4b46cce96d9ce63d13d6a294
Debarko De <debarko89@gmail.com>
2015-08-30 07:24:00 +0530

    Fix:asd

--- common/models/container.js
+++ common/models/container.js
@@ -1,8 +1,3 @@
 module.exports = function(Container) {
-	Container.beforeRemote('**', function( ctx, modelInstance, next) { 
-		console.log('111');
-		console.log(ctx);
-		console.log(modelInstance);
-	 next();
-	});
+
 };
